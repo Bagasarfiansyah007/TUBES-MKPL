fccabe1025d455a2c384ab5687ecf1619e15574d
Kevin Reihan Ghazali Adam <55223583+kevinpakev@users.noreply.github.com>
2022-06-30 21:58:28 +0700

    Create BangunDatarTest.java

---
+++ src/jurnal_mod7_1302194007_nkr/BangunDatarTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BangunDatarTest {
+    
+    @Test
+    public void testHitungLuas() {
+        Lingkaran_1302194007 test = new Lingkaran_1302194007("test",2);
+        double expect = 12.56;
+        double actual = test.hitungLuas();
+        assertEquals(expect,actual);
+    }
+    
+}
